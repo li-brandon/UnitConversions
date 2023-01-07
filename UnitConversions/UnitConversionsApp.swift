6d2ab0d0b0d3e6888ba1836d6f1d0310c637ccbe
Brandon Li <li.brandon@outlook.com>
2023-01-07 10:50:46 -0600

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Brandon Li on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
